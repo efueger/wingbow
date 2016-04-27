a0f6e07054b3ff23e40fa065f1b2d606762a8c78
James Whitney <james@whitney.io>
2016-04-27 18:35:33 +1000

    feat(collection): add additional methods
    Add `chunk`, `compact`, `flatten`, and `remove`.

--- src/wingbow/collection/collection.ts
+++ src/wingbow/collection/collection.ts
@@ -1,7 +1,7 @@
 import { Jsonable } from '../utils/types';
 import { toNumberOrNaN } from '../utils/to';
 import { IllegalOperatorError } from '../collection/errors';
-import { isArrayLike, isFunction } from '../utils/is';
+import { isArrayLike, isFunction, isNumber } from '../utils/is';
 import { ExtendableArray, ExtendableArrayInstance } from './extendable-array';
 
 export interface CollectionInstance<T> extends ExtendableArrayInstance<T> {
@@ -16,12 +16,71 @@ export interface Collection<T> {
 
 export class Collection<T> extends ExtendableArray<T> {
 
+    public chunk(size :number = 1) {
+        const arr = [];
+        let index = 0;
+        const len = this.length;
+        while (index < len) {
+            let partial = this.slice(index, (index += size));
+            arr.push(partial);
+        }
+        const Ctor = this.constructor[Symbol.species];
+        const result = new Ctor(...arr);
+        return result;
+    }
+
+    public compact() {
+        return this.filter(Boolean);
+    }
+
     public first() :T {
         return this[0];
     }
 
+    public flatten() {
+        const arr = _flatten(this, []);
+        const Ctor = this.constructor[Symbol.species];
+        const result = new Ctor(...arr);
+        return result;
+        ////////////////////
+        function _flatten(item, _arr) {
+            let index = -1;
+            const len = item.length;
+            while (++index < len) {
+                const value = item[index];
+                if (isArrayLike(value)) {
+                    _flatten(value, _arr);
+                } else {
+                    _arr.push(value);
+                }
+            }
+            return _arr;
+        }
+    }
+
     public last() :T {
         return this[this.length - 1];
+    }
+
+    public remove(predicate :(value :T, index :number, obj :this) => boolean, thisArg? :any);
+    public remove(predicate :number, thisArg? :any);
+    public remove(predicate, thisArg) {
+        if (isNumber(predicate)) {
+            let index :number = predicate;
+            if (index < 0) {
+                index += this.length;
+            }
+            predicate = (v, i) => i === index;
+        }
+        const arr = [];
+        this.forEach((value, index, obj) => {
+            if (!predicate(value, index, obj)) {
+                arr.push(value);
+            }
+        }, thisArg);
+        const Ctor = this.constructor[Symbol.species];
+        const result = new Ctor(...arr);
+        return result;
     }
 
     public where(key? :string, operator? :Function, value? :Jsonable);
--- test/unit/specs/wingbow/collection/collection.spec.ts
+++ test/unit/specs/wingbow/collection/collection.spec.ts
@@ -43,6 +43,53 @@ describe(`Collection`, () => {
 
     });
 
+    describe(`chunk`, () => {
+
+        it(`should chop up the collection into smaller bits`, () => {
+            expect(col.chunk(2) instanceof Collection).toBe(true);
+            expect(col.chunk(2)).toEqual(new Collection<Collection<TestCollection>>(
+                new Collection<TestCollection>({id: 0, value: `foo`}, {id: 1, value: 0}),
+                new Collection<TestCollection>({id: 2, value: 1}, {id: 3, value: null}),
+                new Collection<TestCollection>({id: 4, value: undefined}, {id: 5, value: true}),
+                new Collection<TestCollection>({id: 6, value: false}, {id: 7, value: `bar`})
+            ));
+        });
+
+        it(`should handle remainders`, () => {
+            expect(col.chunk(3) instanceof Collection).toBe(true);
+            expect(col.chunk(3)).toEqual(new Collection<Collection<TestCollection>>(
+                new Collection<TestCollection>({id: 0, value: `foo`}, {id: 1, value: 0}, {id: 2, value: 1}),
+                new Collection<TestCollection>({id: 3, value: null}, {id: 4, value: undefined}, {id: 5, value: true}),
+                new Collection<TestCollection>({id: 6, value: false}, {id: 7, value: `bar`})
+            ));
+        });
+
+        it(`should default to chopping up every "1" item(s)`, () => {
+            expect(col.chunk() instanceof Collection).toBe(true);
+            expect(col.chunk()).toEqual(new Collection<Collection<TestCollection>>(
+                new Collection<TestCollection>({id: 0, value: `foo`}),
+                new Collection<TestCollection>({id: 1, value: 0}),
+                new Collection<TestCollection>({id: 2, value: 1}),
+                new Collection<TestCollection>({id: 3, value: null}),
+                new Collection<TestCollection>({id: 4, value: undefined}),
+                new Collection<TestCollection>({id: 5, value: true}),
+                new Collection<TestCollection>({id: 6, value: false}),
+                new Collection<TestCollection>({id: 7, value: `bar`})
+            ));
+        });
+
+    });
+
+    describe(`compact`, () => {
+
+        it(`should remove all falsy values from the Collection`, () => {
+            col = new Collection<any>(undefined, 0, NaN, 1, false, 2, ``, 3, null, `4`);
+            expect(col.compact() instanceof Collection).toBe(true);
+            expect(col.compact()).toEqual(new Collection<any>(1, 2, 3, `4`));
+        });
+
+    });
+
     describe(`first`, () => {
 
         it(`should return the "first" element in the Collection`, () => {
@@ -51,10 +98,57 @@ describe(`Collection`, () => {
 
     });
 
+    describe(`flatten`, () => {
+
+        it(`should remove nested Arrays and Collections`, () => {
+            col = new Collection<any>(1, [[2], [new Collection<any>(3, 4), [5, [6, new Collection<any>(7)]], new Collection<any>(8, 9, 10)], new Collection<any>(11)], [12]);
+            expect(col.flatten() instanceof Collection).toBe(true);
+            expect(col.flatten()).toEqual(new Collection<any>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12));
+        });
+
+    });
+
     describe(`last`, () => {
 
         it(`should return the "last" element in the Collection`, () => {
             expect(col.last()).toEqual({id: 7, value: `bar`});
+        });
+
+    });
+
+    describe(`remove`, () => {
+
+        it(`should be able to remove an item by an "index"`, () => {
+            expect(col.remove(3) instanceof Collection).toBe(true);
+            expect(col.remove(3)).toEqual(new Collection<TestCollection>(
+                {id: 0, value: `foo`},
+                {id: 1, value: 0},
+                {id: 2, value: 1},
+                {id: 4, value: undefined},
+                {id: 5, value: true},
+                {id: 6, value: false},
+                {id: 7, value: `bar`}
+            ));
+            expect(col.remove(-2) instanceof Collection).toBe(true);
+            expect(col.remove(-2)).toEqual(new Collection<TestCollection>(
+                {id: 0, value: `foo`},
+                {id: 1, value: 0},
+                {id: 2, value: 1},
+                {id: 3, value: null},
+                {id: 4, value: undefined},
+                {id: 5, value: true},
+                {id: 7, value: `bar`}
+            ));
+        });
+
+        it(`should be able to remove an item by a "predicate"`, () => {
+            expect(col.remove((v, i) => i % 2 === 0) instanceof Collection).toBe(true);
+            expect(col.remove((v, i) => i % 2 === 0)).toEqual(new Collection<TestCollection>(
+                {id: 1, value: 0},
+                {id: 3, value: null},
+                {id: 5, value: true},
+                {id: 7, value: `bar`}
+            ));
         });
 
     });
